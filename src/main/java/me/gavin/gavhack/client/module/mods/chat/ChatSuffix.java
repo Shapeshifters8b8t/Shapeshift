364f716f97454483c030a3c12707f47dff98831e
AcaiBerii <studiouifxdesignersandcoders@gmail.com>
2021-05-10 14:44:42 -0700

    Beriiclient ontoppe

--- src/main/java/me/gavin/gavhack/client/module/mods/chat/ChatSuffix.java
+++ src/main/java/me/gavin/gavhack/client/module/mods/chat/ChatSuffix.java
@@ -3,17 +3,19 @@ package me.gavin.gavhack.client.module.mods.chat;
 import me.gavin.gavhack.BeriiOnToppe;
 import me.gavin.gavhack.client.module.Category;
 import me.gavin.gavhack.client.module.Module;
+import me.gavin.gavhack.client.setting.impl.ModeSetting;
 import net.minecraft.network.play.client.CPacketChatMessage;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ChatSuffix extends Module {
+    public ModeSetting suffixMode = new ModeSetting("Suffix", this, "Regular", "Regular", "Ontoppe");
     public ChatSuffix() {
         super("ChatSuffix", Category.Chat);
     }
 
-    String suffix = " | BeriiClient";
+
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onChat(ClientChatEvent event) {
@@ -24,7 +26,13 @@ public class ChatSuffix extends Module {
 
         // done to make sure the suffix is not added onto your message
         // history when you press the up arrow in the chat gui
-        mc.getConnection().sendPacket(new CPacketChatMessage(event.getOriginalMessage().concat(suffix)));
+        if (suffixMode.modes.get(suffixMode.index).equals("Regular")) {
+            event.setMessage(event.getOriginalMessage().concat(" | BeriiClient"));
+        }
+        else if (suffixMode.modes.get(suffixMode.index).equals("Ontoppe")) {
+            event.setMessage(event.getOriginalMessage().concat(" | BeriiClient is the on toppest"));
+        }
+
         mc.ingameGUI.getChatGUI().addToSentMessages(event.getOriginalMessage());
     }
 }
