8be85f50017254f83dc76342e7e60278fc132521
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-06-06 12:13:34 -0400

    funi 4

--- src/main/java/dev/is_a/acaiberii/client/client/module/mods/misc/Spammer.java
+++ src/main/java/dev/is_a/acaiberii/client/client/module/mods/misc/Spammer.java
@@ -9,10 +9,13 @@ import dev.is_a.acaiberii.client.client.setting.impl.NumberSetting;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class Spammer extends Module {
-    private final NumberSetting delay = new NumberSetting("Delay", this, 1, 1, 100, 1);
+    public final NumberSetting delay = new NumberSetting("Delay", this, 1, 1, 100, 1);
+    public final BooleanSetting bypass = new BooleanSetting("Bypass", this, true);
 
     private final List<String> messages = new ArrayList<>();
     private int timer;
@@ -20,6 +23,7 @@ public class Spammer extends Module {
 
     public Spammer() {
         super("Spammer", Category.Misc);
+        this.settings.add(delay);
     }
 
     public void onEnable() {
@@ -31,7 +35,10 @@ public class Spammer extends Module {
     private void onTick(LocalTickEvent event) {
         if (messages.isEmpty()) return;
         if (timer <= 0) {
-            ChatManager.sendChatMessage("Shapeshift ontoppe!");
+            if (bypass.enabled)
+                ChatManager.sendChatMessage("Shapeshift ontoppe! " + String.valueOf(new Random().nextInt(99999)));
+            else
+                ChatManager.sendChatMessage("Shapeshift ontoppe!");
             timer = (int) (delay.getValue() * 20);
         } else {
             timer--;
