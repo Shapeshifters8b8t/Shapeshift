f1d8f96394838e7454e6029a84103ee54cb5e693
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-06-06 21:14:45 -0400

    aaaa

--- src/main/java/dev/is_a/acaiberii/client/client/module/mods/chat/ChatSuffix.java
+++ src/main/java/dev/is_a/acaiberii/client/client/module/mods/chat/ChatSuffix.java
@@ -7,6 +7,7 @@ import dev.is_a.acaiberii.client.client.setting.impl.ModeSetting;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.commons.lang3.StringEscapeUtils;
 
 public class ChatSuffix extends Module {
 
@@ -18,7 +19,7 @@ public class ChatSuffix extends Module {
     public void onChat(ClientChatEvent event) {
         if (event.getMessage().startsWith("/") || event.getMessage().startsWith(String.valueOf(BeriiOnToppe.commandManager.prefix)))
             return;
-        event.setMessage(event.getMessage().concat(" | \uD835\uDE82\uD835\uDE91\uD835\uDE8A\uD835\uDE99\uD835\uDE8E\uD835\uDE9C\uD835\uDE91\uD835\uDE92\uD835\uDE8F\uD835\uDE9D"));
+        event.setMessage(event.getMessage().concat(StringEscapeUtils.unescapeCsv(" | shapeshift")));
         mc.ingameGUI.getChatGUI().addToSentMessages(event.getOriginalMessage());
     }
 }
